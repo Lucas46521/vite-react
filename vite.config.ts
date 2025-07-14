2bc199faa77ca22a9e615520f31d199567b6d2a0
Lucas <111234099+Lucas46521@users.noreply.github.com>
2025-07-13 22:08:13 -0300

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  publicDir: 'public',
-  assetsInclude: ['**/*.mdx'],
-  server: {
-    host: '0.0.0.0',
-    port: 5000
-  },
-  build: {
-    rollupOptions: {
-      input: {
-        main: './index.html'
-      }
-    }
-  }
+  plugins: [react()]
 })
